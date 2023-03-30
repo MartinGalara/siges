df2346c5cc23c5e1d47ca33927a5a72308e5c81c
Martin Galara <mgalara@gmail.com>
2023-03-30 14:17:39 -0300

    cambio prop de user

--- sigesback/src/models/User.js
+++ sigesback/src/models/User.js
@@ -12,7 +12,7 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false
         },
-        address: {
+        info: {
             type: DataTypes.STRING,
             allowNull: false,
         }
