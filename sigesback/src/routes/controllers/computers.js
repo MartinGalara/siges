76e8786f7e26d102e10b98d7afe010f1b2a7d8b4
Martin Galara <mgalara@gmail.com>
2023-03-31 15:14:18 -0300

    get computers

--- sigesback/src/routes/controllers/computers.js
+++ sigesback/src/routes/controllers/computers.js
@@ -1,10 +1,30 @@
 const { Router } = require('express');
+const { Computer } = require('../../db.js')
 
 const router = Router();
 
 router.get('/', async (req, res) => {
 
+    const {userId} = req.query;
 
+    try {
+
+        if(userId){
+            const computers = await Computer.findAll({
+                where:{
+                    userId: userId
+                }
+            })
+
+            return res.status(200).json(computers)
+        }
+
+        const allComputers = await Computer.findAll()
+
+        return res.status(200).json(allComputers)
+    } catch (error) {
+         return res.status(400).send(error.message)
+    }
 })
 
 module.exports = router;
