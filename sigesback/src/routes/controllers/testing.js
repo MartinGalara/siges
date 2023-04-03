e5fbf359b985656c4dccbe90f990a5ebc768248c
Martin Galara <mgalara@gmail.com>
2023-04-03 14:47:05 -0300

    testing

--- sigesback/src/routes/controllers/testing.js
+++ sigesback/src/routes/controllers/testing.js
@@ -5,21 +5,52 @@ const { User } = require('../../db.js')
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const {email} = req.query
-    try {
-        if(email){
-            const user = await User.findAll({
-                where:{
-                    email: email
-                }
-            })
-        return res.status(200).json(user)
+    const asdasd = [
+        "DT7602",
+        "ZH7869",
+        "AS6496",
+        "XK7499",
+        "QT3048",
+        "XU0641",
+        "MN7809",
+        "WD7212",
+        "UP4332",
+        "UC0189",
+        "VX4848",
+        "IY6068",
+        "QN2356",
+        "CE8676",
+        "MS1425",
+        "FZ5361",
+        "XP4360",
+        "YT2140",
+        "PW4157",
+        "PX4921",
+        "WQ8404",
+        "UT7218",
+        "LX6713",
+        "ZF4816",
+        "KC2678",
+        "BZ5386",
+        "RI0891",
+        "AM4503",
+        "HE5459",
+        "CY2719",
+        "CW9340"
+    ]
+    
+    for (let i = 0; i < asdasd.length; i++) {
+        try {
+            const userToDelete = await User.findByPk(e)
+            console.log(userToDelete)
+            
+        } catch (error) {
+            console.log(error)
         }
-        const allUsers = await User.findAll();
-        return res.status(200).json(allUsers)
-    } catch (error) {
-        return res.status(400).send(error.message)
+        
     }
+
+    return res.status(200).send("listo")
     
 })
 
