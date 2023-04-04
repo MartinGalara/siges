98da98a53d69be1055a4eec2baa07f5702acbb87
Martin Galara <mgalara@gmail.com>
2023-04-04 19:09:05 -0300

    getall

--- sigesback/src/routes/controllers/testing.js
+++ sigesback/src/routes/controllers/testing.js
@@ -1,71 +1,15 @@
 const { Router } = require('express');
-const { User } = require('../../db.js')
+const { Testing } = require('../../db.js')
 const { Computer } = require('../../db.js')
 //const userExtractor = require('../middleware/userExtractor.js.js');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    /* const asdasd = [
-        "DT7602",
-        "ZH7869",
-        "AS6496",
-        "XK7499",
-        "QT3048",
-        "XU0641",
-        "MN7809",
-        "WD7212",
-        "UP4332",
-        "UC0189",
-        "VX4848",
-        "IY6068",
-        "QN2356",
-        "CE8676",
-        "MS1425",
-        "FZ5361",
-        "XP4360",
-        "YT2140",
-        "PW4157",
-        "PX4921",
-        "WQ8404",
-        "UT7218",
-        "LX6713",
-        "ZF4816",
-        "KC2678",
-        "BZ5386",
-        "RI0891",
-        "AM4503",
-        "HE5459",
-        "CY2719",
-        "CW9340"
-    ]
     
-    for (let i = 0; i < asdasd.length; i++) {
-        try {
-            const userToDelete = await User.findByPk(asdasd[i])
-            await userToDelete.destroy()
-            
-        } catch (error) {
-            console.log(`${userToDelete} no pudo ser borrado`)
-        }
-        
-    }
+    const allTesting = Testing.findAll()
 
-    return res.status(200).send("listo") */
-    
-    const allComputers = await Computer.findAll();
-
-    for (let i = 0; i < allComputers.length; i++) {
-        
-        let newAlias = allComputers[i].alias.charAt(0).toUpperCase() + allComputers[i].alias.slice(1);
-
-        const pcToUpdate = await Computer.findByPk(allComputers[i].id)
-
-        await pcToUpdate.update({ 
-            alias: newAlias
-        });
-        
-    }
+    return res.status(200).json(allTesting)
 
 })
 
