1065dc02c815f0f2914b9ab88fe252362bf8be29
Martin Galara <mgalara@gmail.com>
2023-04-13 14:09:03 -0300

    asd

--- sigesfront/src/components/LandingPage/LandingPage.jsx
+++ sigesfront/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
     <div>
       <h1>Welcome to Henry Videogames</h1>
-{/*       <Link to={"/home"}>
-        <button className={s.myButton}>Start</button>
-      </Link> */}
     </div>
   );
 }
