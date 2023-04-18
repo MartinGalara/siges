4a5492a702d81b25098bc26bd83046ccbaa2de99
Martin Galara <mgalara@gmail.com>
2023-04-18 10:33:31 -0300

    bien hecho

--- sigesback/src/routes/controllers/computers.js
+++ sigesback/src/routes/controllers/computers.js
@@ -3,18 +3,32 @@ const { Computer,User } = require('../../db.js')
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
 
-    const {userId} = req.query;
     const {id} = req.params;
     console.log(id)
 
     try {
-
         if(id){
             const computer = await Computer.findByPk(id)
             return res.status(200).json(computer)
         }
+        else{
+            return res.status(400).send("Missing id")
+        }
+    } catch (error) {
+        console.log(error.message)
+        return res.status(400).send(error.message)
+    }
+
+})
+
+
+router.get('/', async (req, res) => {
+
+    const {userId} = req.query;
+
+    try {
 
         if(userId){
             const computers = await Computer.findAll({
